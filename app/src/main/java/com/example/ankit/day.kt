3a1cc4315c2c48ebca3601595f9d8e3f0f2c0e0c
khambya <dabc75945@icloud.com>
2025-11-03 19:46:47 +0545

    this is the first commit for the kotlin-lecture and pushing the file day by day as i go through course

---
+++ app/src/main/java/com/example/ankit/day.kt
@@ -0,0 +1,14 @@
+package com.example.ankit
+
+fun main() {
+    //immutable
+    var name : String = "gautam"
+    name = "ankit"
+
+    //mutable
+    val age : Int = 23
+
+    println(" my name is ${name.uppercase()} and my age is $age")
+
+
+}
